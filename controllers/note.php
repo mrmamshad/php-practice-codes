693f16ffe1c3ad30e9ac40afac57c6ce2a09e865
mrmamshad <mamshad3106@gmail.com>
2024-03-19 11:27:29 +0600

    switch for user_id2

--- controllers/note.php
+++ controllers/note.php
@@ -3,12 +3,26 @@
 $config = require 'config.php';
 $db = new Database($config['database']);
 
-
+$currentUserId = 2;
 // that is the id of notes we want to grab from database 
 
-$id = $_GET['id'];
+// $id = $_GET['id'];
 
-$notes = $db->query('SELECT * FROM `notes` WHERE id = :id', ['id' => $id]);
+$notes = $db->query('select * from notes where id = :id', [
+    'id' => $_GET['id']
+]);
+
+if (!$notes) {
+    abort();
+};
+// dd($notes);
+
+
+if ($notes[0]["user_id"] !== $currentUserId) {
+    abort(403);
+}
+
+
 // Explanation :
 //         select sobkichu from notes table where id = akta value , jai value ta 'id' namok array key r value 
 
--- controllers/notes.php
+++ controllers/notes.php
@@ -3,7 +3,7 @@
 $config = require 'config.php';
 $db = new Database($config['database']);
 
-$notes = $db->query('SELECT * FROM `notes` WHERE user_id = 1');
+$notes = $db->query('SELECT * FROM `notes` WHERE user_id = 2');
 
 // foreach ($notes as $post) {
 //     echo "<li>" . $post['body'] . "</li>";
