6ed319332c26934b25ceb3a05a80ede52f0a1cdb
mrmamshad <mamshad3106@gmail.com>
2024-03-21 01:37:02 +0600

    connected _POST to database table

--- controllers/create-note.php
+++ controllers/create-note.php
@@ -2,9 +2,16 @@
 
 $heading = 'create note.';
 
+$config = require('config.php');
+$db = new Database($config['database']);
+
 // dd($_SERVER);
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO `notes` (`body`, `user_id`) VALUES
+(:body, :user_id)', [
+        'body' => $_POST['about'],
+        'user_id' => 2,
+    ]);
 }
 
 require 'views/note-ceate.view.php';
