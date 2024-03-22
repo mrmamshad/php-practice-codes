fe9313a6e8e417b0dca282ce9ec4afd18c66cadb
mrmamshad <mamshad3106@gmail.com>
2024-03-22 09:31:25 +0600

    learning new things

--- views/notes.view.php
+++ views/notes.view.php
@@ -8,14 +8,14 @@
             <?php foreach ($notes as $note) : ?>
                 <li>
                     <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-                        <?= $note['body'] ?>
+                        <?= htmlspecialchars($note['body']) ?>
                     </a>
                 </li>
             <?php endforeach; ?>
         </ul>
 
         <p class="mt-6">
-            <a href="/notes/create" class="text-green-600  underline ">Create Note</a>
+            <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
         </p>
     </div>
 </main>
